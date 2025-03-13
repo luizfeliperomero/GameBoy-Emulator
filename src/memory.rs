3f218d792b2c714d333a1ab796f17b56322347c4
luizfeliperomero <luizfeliperomero10@gmail.com>
2025-03-13 17:15:44 -0300

    Refactor Memory struct

--- src/memory.rs
+++ src/memory.rs
@@ -1,42 +1,47 @@
-const MEMORY_SIZE: u16 = 2.pow(16);
+use std::fs;
+use std::error::Error;
+
+const MEMORY_SIZE: usize = 2_usize.pow(16);
 struct Range {
-    start: u8,
-    end: u8,
+    start: u16,
+    end: u16,
 }
 impl Range {
-    fn new(start: u8, end: u8) -> Self {
+    fn new(start: u16, end: u16) -> Self {
         Self {
             start,
             end
         }
     }
 }
-enum MemoryMap {
-    ROM(Range),
-    VRAM(Range),
-    ExternalRAM(Range),
-    WorkRAM(Range),
-    OAM(Range),
-    IO(Range),
-    HRAM(Range),
+struct MemoryMap {
+    rom: Range,
+    v_ram: Range,
+    external_ram: Range,
+    work_ram: Range,
+    oam: Range,
+    io: Range,
+    h_ram: Range,
 }
 pub struct Memory {
     memory: [u8; MEMORY_SIZE],
-    map: [MemoryMap, 7]
+    map: MemoryMap,
+    rom_size: usize
 }
 impl Memory {
     pub fn new() -> Self {
         Self {
             memory: [0u8; MEMORY_SIZE],
-            maps: [
-                ROM(Range::new(0x0000, 0x7FFF)),
-                VRAM(Range::new(0x8000, 0x9FFF)),
-                ExternalRAM(Range::new(0xA000, 0xBFFF)),
-                WorkRAM(Range::new(0xC000, 0xDFFF)),
-                OAM(Range::new(0xFE00, 0xFE9F)),
-                IO(Range::new(0xFF00, 0xFF7F)),
-                HRAM(Range::new(0xFF80, 0xFFFE)),
-            ]
+            map: MemoryMap {
+                rom: Range::new(0x0000, 0x7FFF),
+                v_ram: Range::new(0x8000, 0x9FFF),
+                external_ram: Range::new(0xA000, 0xBFFF),
+                work_ram: Range::new(0xC000, 0xDFFF),
+                oam: Range::new(0xFE00, 0xFE9F),
+                io: Range::new(0xFF00, 0xFF7F),
+                h_ram: Range::new(0xFF80, 0xFFFE),
+            },
+            rom_size: 0
         }
     }
 }
